9b07dcc454276754e12b023b18d3f7bd34ccdf8d
yaluknov <luk-alex04@yandex.ru>
2026-03-08 14:21:55 +0300

    Настройка темы

--- app/src/main/java/com/example/taskmanager/ui/theme/Color.kt
+++ app/src/main/java/com/example/taskmanager/ui/theme/Color.kt
@@ -2,10 +2,52 @@ package com.example.taskmanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary10 = Color(0xFF001848)
+val Primary20 = Color(0xFF002D8A)
+val Primary30 = Color(0xFF003DAF)
+val Primary40 = Color(0xFF0C51C0)
+val Primary80 = Color(0xFFADC6FF)
+val Primary90 = Color(0xFFD6E3FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val Secondary10 = Color(0xFF0A1A33)
+val Secondary20 = Color(0xFF162E57)
+val Secondary30 = Color(0xFF24457C)
+val Secondary40 = Color(0xFF2C5DA3)
+val Secondary80 = Color(0xFFA8C3F5)
+val Secondary90 = Color(0xFFD6E4FF)
+
+
+val Error10 = Color(0xFF410002)
+val Error20 = Color(0xFF690005)
+val Error30 = Color(0xFF93000A)
+val Error40 = Color(0xFFBA1A1A)
+val Error80 = Color(0xFFFFB4AB)
+val Error90 = Color(0xFFFFDAD6)
+
+
+val Neutral10 = Color(0xFF1A1A1A)
+val Neutral20 = Color(0xFF2C2C2C)
+val Neutral90 = Color(0xFFE3E3E3)
+val Neutral95 = Color(0xFFF1F1F1)
+
+
+val NeutralVariant30 = Color(0xFF434343)
+val NeutralVariant50 = Color(0xFF737373)
+val NeutralVariant60 = Color(0xFF8F8F8F)
+val NeutralVariant70 = Color(0xFFBBBBBB)
+
+
+val LightBackground = Color(0xFFEEF2FA)
+val LightSurfaceVariant = Color(0xFFFFFFFF)
+val LightSurfaceContainer = Color(0xFFE6ECF5)
+val LightOutline = Color(0xFFB8C5D8)
+val LightOnSurfaceVariant = Color(0xFF434343)
+
+
+val DarkBackground = Color(0xFF12151C)
+val DarkSurface = Color(0xFF12151C)
+val DarkSurfaceVariant = Color(0xFF1E2333)
+val DarkSurfaceContainer = Color(0xFF1A1E2B)
+val DarkOnSurfaceVariant = Color(0xFFBBC4D8)
+val DarkOutline = Color(0xFF3A4258)
