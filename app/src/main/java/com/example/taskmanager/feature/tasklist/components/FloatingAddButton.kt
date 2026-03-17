1623d91c1ec43f6dc106373b6e48a6b0337adf47
yaluknov <luk-alex04@yandex.ru>
2026-03-17 18:06:50 +0300

    Кнопка опущена ближе к навигационному бару

--- app/src/main/java/com/example/taskmanager/feature/tasklist/components/FloatingAddButton.kt
+++ app/src/main/java/com/example/taskmanager/feature/tasklist/components/FloatingAddButton.kt
@@ -1,16 +1,20 @@
 package com.example.taskmanager.feature.tasklist.components
 
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FloatingAddButton(
     onClick: () -> Unit,
 ) {
     FloatingActionButton(
+        modifier = Modifier.offset(y = 24.dp),
         onClick = onClick,
         content = {
             Icon(
