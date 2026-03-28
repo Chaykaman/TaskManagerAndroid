d7733d2650bfe05efd8f615aeac67ac4c86e0563
aiui338 <dima.pruzhina@bk.ru>
2026-03-28 17:05:33 +0300

    Добавление миграции в MainDb

--- app/src/main/java/com/example/taskmanager/data/local/database/MainDb.kt
+++ app/src/main/java/com/example/taskmanager/data/local/database/MainDb.kt
@@ -6,14 +6,19 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.taskmanager.data.local.dao.Dao
+import com.example.taskmanager.data.local.dao.HabitDao
 import com.example.taskmanager.data.local.dao.SurveyDao
 import com.example.taskmanager.data.local.dao.TaskDao
+import com.example.taskmanager.data.local.entity.Habit
+import com.example.taskmanager.data.local.entity.HabitLog
 import com.example.taskmanager.data.local.entity.SurveyResult
 import com.example.taskmanager.data.local.entity.Task
+import com.example.taskmanager.data.local.database.migrations.MIGRATION_1_2
+import com.example.taskmanager.data.local.database.migrations.MIGRATION_2_3
 
 @Database(
-    entities = [Task::class, SurveyResult::class],
-    version = 2,
+    entities = [Task::class, SurveyResult::class, Habit::class, HabitLog::class],
+    version = 3,  // последняя версия базы
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -22,6 +27,7 @@ abstract class MainDb : RoomDatabase() {
     abstract fun getDao(): Dao
     abstract fun getTaskDao(): TaskDao
     abstract fun getSurveyDao(): SurveyDao
+    abstract fun getHabitDao(): HabitDao
 
     companion object {
         @Volatile
@@ -34,10 +40,8 @@ abstract class MainDb : RoomDatabase() {
                     MainDb::class.java,
                     "task.db"
                 )
-                    .fallbackToDestructiveMigration(true)
-                    .build().also {
-                        INSTANCE = it
-                    }
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                    .build().also { INSTANCE = it }
             }
         }
     }
